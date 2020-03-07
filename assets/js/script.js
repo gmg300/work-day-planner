5b3b06a68efd34e53545f2fca4bea2232f6a4740
Garrett <garrettgassensmith@gmail.com>
2020-03-07 15:55:16 -0600

    buttons now update based on what's in the textarea and push to the schedule which gets pushed to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,44 @@
 $(document).ready(function() {
-  init();
-
   var schedule = [
     {
       hour: "9am",
-      event: [
-
-      ]
+      event: []
     },
     {
       hour: "10am",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "11am",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "12pm",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "1pm",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "2pm",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "3pm",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "4pm",
-      event: [
-        
-      ]
+      event: []
     },
     {
       hour: "5pm",
-      event: [
-        
-      ]
+      event: []
     }
   ];
 
-  function init() {
-    renderDate();
-    // getSchedule();
-  }
+  renderDate();
 
   function renderDate() {
     var currentDate = moment().format("dddd[ | ]MMMM D, h:mm:ss");
@@ -71,42 +48,58 @@ $(document).ready(function() {
     setInterval(renderDate, 4);
   }
 
-  // Store schedule
-  localStorage.setItem('schedule', JSON.stringify(schedule));
+  getSchedule();
 
-  // Get schedule
-  var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
-  if (storedSchedule !== null) {
-    schedule = storedSchedule;
+  function getSchedule() {
+    var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
+    if (storedSchedule !== null) {
+      schedule = storedSchedule;
+    }
+    renderSchedule();
   }
 
-  // Render schedule
-  // Cannot for the life of me get this into a working function, it only works outside a function or when I bring schedule directly in
-  schedule.forEach((item) => {
-    console.log(item)
-    var block = 
-      `<tr data-index="${item}" class="d-flex mb-1 shadow-sm rounded-right">
-          <th scope="row" class="col-2 --border-left">${item.hour}</th>
-          <td class="col-10 p-0">
-            <div class="input-group">
-            <textarea class="form-control event-text">${item.event}</textarea>
-            <div class="input-group-append">
-              <button class="update-btn btn d-flex justify-center align-center">
-                <i class="px-2 fas fa-2x fa-sync-alt"></i>
-              </button>
-            </div>
-            </div>
-          </td>
-        </tr>`;
-    $("tbody").append(block);
+  function renderSchedule() {
+    $("tbody").empty();
+    schedule.forEach((item, i) => {
+      var block = `<tr data-index="${i}" class="hour-block d-flex mb-1 shadow-sm rounded-right">
+            <th scope="row" class="col-2 --border-left">${item.hour}</th>
+            <td class="col-10 p-0">
+              <div class="input-group">
+                <textarea class="form-control event-text">${item.event}</textarea>
+                <div class="input-group-append">
+                  <button class="update-btn btn d-flex justify-center align-center">
+                    <i class="px-2 fas fa-2x fa-sync-alt"></i>
+                  </button>
+                </div>
+              </div>
+            </td>
+          </tr>`;
+      $("tbody").append(block);
+    });
+  }
+
+  function storeSchedule() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+  }
+
+  $(document).on("click", ".update-btn", function(e) {
+    e.preventDefault();
+    // Find the new event text
+    var content = $(this)
+      .closest(".input-group")
+      .find(".event-text")
+      .val();
+    // Find the spot in the schedule where it needs to go
+    var index = $(this)
+      .closest(".hour-block")
+      .attr("data-index");
+    var target = schedule[index].event;
+    // Push the content to the schedule or replace old event at selected index
+    if(target.length > 0) {
+      target.splice(0, target.length);
+    }
+    target.push(content);
+    storeSchedule();
+    renderSchedule();
   });
-
-  //   function storeSchedule() {}
-
-  //   function clearSchedule() {}
-
-  $('.update-btn').on('click', function(e) {
-    e.preventDefault()
-    console.log($(this).find('event-text'))
-  })
 });
